17fe389989075613cee19198aeee97bd1704a773
Saswata Mukherjee <saswataminsta@yahoo.com>
2021-02-20 17:47:46 +0530

    Create resolver
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,11 @@ import "reflect-metadata";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
+import { ChatResolver } from "./resolvers/chat";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const main = async () => {
   const app = express();
---
+++ server/src/resolvers/chat.ts
@@ -0,0 +1,5 @@
+import { Resolver } from "type-graphql";
+import { Chat } from "../entities/Chat";
+
+@Resolver(Chat)
+export class ChatResolver {}
